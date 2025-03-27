0fdde396c0d96ef828757acb01a78b49a342a507
Ragul198 <ragul198a@gmail.com>
2025-03-27 14:59:54 +0530

    Front_end_done

--- youtube_clone/src/componenets/Recomended/Recomended.jsx
+++ youtube_clone/src/componenets/Recomended/Recomended.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 import './Recomended.css'
+import thumbnail1 from '../../assets/thumbnail1.png';
+import thumbnail2 from '../../assets/thumbnail2.png';
+import thumbnail3 from '../../assets/thumbnail3.png';
+import thumbnail4 from '../../assets/thumbnail4.png';
+import thumbnail5 from '../../assets/thumbnail5.png';
+import thumbnail6 from '../../assets/thumbnail6.png';
+import thumbnail7 from '../../assets/thumbnail7.png';
+import thumbnail8 from '../../assets/thumbnail8.png';
 
 const Recomended = () => {
   return (
-    <div>Recomended</div>
+    <div className="recomended">
+     <div className="video">
+      <img src={thumbnail1} alt="thumbnail1" />
+      <div className="info">
+        <h3>THe way of water avatar trialer in |tamil|english</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    <div className="video">
+      <img src={thumbnail2} alt="thumbnail1" />
+      <div className="info">
+        <h3>Title of video</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    <div className="video">
+      <img src={thumbnail3} alt="thumbnail1" />
+      <div className="info">
+        <h3>Title of video</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    <div className="video">
+      <img src={thumbnail4} alt="thumbnail1" />
+      <div className="info">
+        <h3>Title of video</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    <div className="video">
+      <img src={thumbnail5} alt="thumbnail1" />
+      <div className="info">
+        <h3>Title of video</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    <div className="video">
+      <img src={thumbnail6} alt="thumbnail1" />
+      <div className="info">
+        <h3>Title of video</h3>
+        <h4>Channel name</h4>
+        <p>18 M Views</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
--- youtube_clone/src/pages/Video_player/Video_player.jsx
+++ youtube_clone/src/pages/Video_player/Video_player.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Video_player.css'
 import Play_video from '../../componenets/playvideo/Play_video'
+import Recomended from '../../componenets/Recomended/Recomended'
 const Video_player = () => {
   return (
     <div className='play-container'>
       <Play_video/>
+      <Recomended/>
       
     </div>
   )
